5957d12ae1b6819f59118a73b36e7ac777d941cd
adekmzrk <adekmzrk@gmail.com>
2026-02-14 13:38:21 +0700

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: process.env.VITE_BASE || '/',
+	base: '/',
 	plugins: [react()],
 	resolve: {
 		alias: {
